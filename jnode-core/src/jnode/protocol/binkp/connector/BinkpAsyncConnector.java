76dd1d74476bf415dc744496d6afc7570e26c8d3
Dmitry Protasov <thodin@thodin.net>
2025-06-30 20:56:26 +0300

    Make logs less verbose

--- jnode-core/src/jnode/protocol/binkp/connector/BinkpAsyncConnector.java
+++ jnode-core/src/jnode/protocol/binkp/connector/BinkpAsyncConnector.java
@@ -94,8 +94,7 @@ public class BinkpAsyncConnector extends BinkpAbstractConnector {
 				try {
 					selector.select(staticMaxTimeout);
 					loopCounter++;
-					logger.l5("=== Selector loop #" + loopCounter + ", keys=" + selector.selectedKeys().size());
-					for (SelectionKey key : selector.selectedKeys()) {
+						for (SelectionKey key : selector.selectedKeys()) {
 						SocketChannel channel = (SocketChannel) key.channel();
 						if (key.isValid()) {
 							if (key.isConnectable()) {
